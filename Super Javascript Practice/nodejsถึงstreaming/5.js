682c1ff284cb3f3ea5060819c154ac5c38e7535d
FALLENSLAYER007 <37149762+POOHPHE@users.noreply.github.com>
2020-05-30 09:42:34 +0700

    Add files via upload

---
+++ Super Javascript Practice/nodejsถึงstreaming/5.js
@@ -0,0 +1,8 @@
+var fs = require('fs')
+
+function finishedReading(error, movieData) {
+  if (error) return console.error(error)
+  // do something with the movieData
+}
+
+fs.readFile('movie.mp4', finishedReading)
